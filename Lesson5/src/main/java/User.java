72127a02730362ea96fbbcda88d428edb1c6c6ae
Egoryus <egoredihanov@gmail.com>
2022-05-19 17:18:49 +0300

    Lesson5, task1, complete

--- Lesson5/src/main/java/User.java
+++ Lesson5/src/main/java/User.java
@@ -4,11 +4,15 @@ public class User {
     int age;
     String sex;
 
-    public User(String name, String surname, int age, String sex) {
+    public User(String name, String surname, String sex) {
         this.name = name;
         this.surname = surname;
-        this.age = age;
         this.sex = sex;
+    }
+
+    public User(String name, String surname, int age, String sex) {
+        this(name, surname, sex);
+        this.age = age;
 
     }
     public String fullName() {
@@ -24,10 +28,12 @@ public class User {
                 ", sex='" + sex + '\'' +
                 '}';
     }
+    @Deprecated
     public void celebrateBirthday(){
         age += 1;
     }
     public void printUser(){
         System.out.println(this.toString());
     }
+
 }
